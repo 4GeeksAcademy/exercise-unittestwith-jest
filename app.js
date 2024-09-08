8f3d9a4e1552f09e2f7ea7c4928be5894d24d16c
RicardoClaro7 <167456148+RicardoClaro7@users.noreply.github.com>
2024-09-08 17:14:46 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 149.03;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.0072;
+    return valueInPound; 
+}
+
+module.exports = { sum,  fromEuroToDollar, fromDollarToYen, fromYenToPound};
